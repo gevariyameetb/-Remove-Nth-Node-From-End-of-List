07e35ed0ab0c63e30eb87715b507e51a890506ff
GEVARIYA MEET B <87793055+gevariyameetb@users.noreply.github.com>
2022-07-03 22:23:31 +0530

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,9 @@
+# -Remove-Nth-Node-From-End-of-List
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        fast, slow = head, head
+        for _ in range(n): fast = fast.next
+        if not fast: return head.next
+        while fast.next: fast, slow = fast.next, slow.next
+        slow.next = slow.next.next
+        return head
